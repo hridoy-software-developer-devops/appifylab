4956bc626733608e3e2e1bd5b10ed8bd03d57672
softviewitlimited <softviewitlimited@gmail.com>
2026-06-10 20:14:45 +0600

    first commit

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,32 @@
+import { Poppins } from "next/font/google";
+
+export const metadata = {
+  title: "Buddy Script",
+  icons: {
+    icon: "/assets/images/logo-copy.svg",
+  },
+};
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "600", "700", "800"],
+});
+
+import "../public/assets/css/bootstrap.min.css";
+import "../public/assets/css/common.css";
+import "../public/assets/css/main.css";
+import "../public/assets/css/responsive.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+    <body className={poppins.className}>
+    	{children}
+    </body>
+    </html>
+    )
+}
