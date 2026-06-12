6437538a5df465b5ce5e41fc276af7db9d3ee2cc
hridoy-software-developer-devops <ikbal15.1320.japan@gmail.com>
2026-06-12 21:11:57 +0600

    make post

---
+++ backend/app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PostController extends Controller
+{
+   public function index(){
+    return Post::with('user')->latest()->paginate(20);
+   }
+
+   public function store(Request $request)
+    {
+        return Post::create([
+            'user_id'=> auth()->id(),
+            'content'=> $request->content,
+            'visibility'=> $request->visibility
+        ]);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\PostController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -9,5 +10,6 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:api')->group(function(){
     Route::get('/me',[AuthController::class,'me']);
     Route::post('/logout',[AuthController::class,'logout']);
-    // Route::apiResource('posts',PostController::class);
+    Route::get('/posts',[PostController::class,'index']);
+    Route::post('/post/create',[PostController::class,'store']);
 });
